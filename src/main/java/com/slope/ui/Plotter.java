25df087020a1dc3c51b204ada48bbc80726a23b3
tpapata1386 <tpapata1386@gmail.com>
2025-12-25 08:40:03 +0330

    Add collisionDot graph details.

--- src/main/java/com/slope/ui/Plotter.java
+++ src/main/java/com/slope/ui/Plotter.java
@@ -2,6 +2,7 @@ package com.slope.ui;
 
 import javafx.scene.canvas.*;
 import javafx.scene.paint.*;
+import javafx.scene.text.Text;
 import java.util.function.DoubleUnaryOperator;
 
 public class Plotter
@@ -20,13 +21,32 @@ public class Plotter
         return canvas.getHeight() - ( (y + boundary) / (2.0 * boundary) ) * canvas.getHeight();
     }
 
+    private void drawCenteredText( double x, double y, String s )
+    {
+        var context = canvas.getGraphicsContext2D();
+
+        Text textNode = new Text( s );
+        textNode.setFont( context.getFont() );
+
+        context.fillText( s, x - textNode.getLayoutBounds().getWidth()/2, y - textNode.getLayoutBounds().getHeight()/2 );
+    }
+
     public void collisionDot( double atX, double atY )
     {
         var context = canvas.getGraphicsContext2D();
+        Paint prevColor = context.getStroke();
 
         // Drawing a circle starts at it's most corner position.
         // So we have to remove 2.5 from each axis.
         context.fillOval( toScreenX( atX ) - 2.5, toScreenY( atY ) - 2.5, 5, 5 );
+
+        // Draw the positions
+        context.setStroke( Color.GRAY );
+        context.strokeLine( toScreenX( atX ), toScreenY( atY ), toScreenX( atX ), canvas.getHeight() / 2 ); // X axis
+        context.strokeLine( toScreenX( atX ), toScreenY( atY ), canvas.getWidth() / 2 , toScreenY( atY ) ); // Y axis
+
+        drawCenteredText( toScreenX( atX ), (canvas.getHeight() / 2) + 20, String.format("%.3f", atX ) );
+        drawCenteredText( (canvas.getWidth() / 2) - 20, toScreenY( atY ) + 12, String.format("%.3f", atY ) );
     }
 
     public Plotter( DoubleUnaryOperator function, Color color )
